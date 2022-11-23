0280f3414261362c30bd7cda1437ba5328b36bec
Aniket <aniketdhokane2071@gmail.com>
2022-11-23 20:10:06 +0530

    Created delete request

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -56,10 +56,25 @@ const updateUser= async(req,res,next)=>{
     return res.status(200).json({message:"Updated Succesfully"})
 }
 
+const deleteUser= async(req,res,next)=>{
+    const id=req.params.id;
+    let user;
+    try {
+        user= await User.findByIdAndRemove(id);
+    } catch (error) {
+        return next(err);
+    }
+    if(!user){
+        return res.status(500).json({message:"Unable to delete"})
+    }
+    return res.status(200).json({message:"Succesfully Deleted"})
+}
+
 
 exports.getAllUsers=getAllUsers;
 exports.addUser=addUser;
 exports.updateUser =updateUser;
+exports.deleteUser=deleteUser;
 
 // status 422 means unprocessable entity
 // status 201 used when something is siccesfully addedd
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,5 +1,5 @@
 const express= require("express");
-const { getAllUsers ,addUser,updateUser} = require("../controller/user-controller");
+const { getAllUsers ,addUser,updateUser, deleteUser} = require("../controller/user-controller");
 
 
 const router= express.Router();
@@ -7,5 +7,6 @@ const router= express.Router();
 router.get("/",getAllUsers)
 router.post("/", addUser)
 router.put("/:id",updateUser)
+router.delete("/:id",deleteUser)
 
 module.exports=router;
