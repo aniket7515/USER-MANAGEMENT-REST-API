603a00878151972aba0b8386431c3aef470a800d
Aniket <aniketdhokane2071@gmail.com>
2022-11-23 20:02:53 +0530

    Update user

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -37,12 +37,29 @@ const addUser = async(req,res,next)=>{
     return res.status(201).json({user})
 }
 
+const updateUser= async(req,res,next)=>{
+    const id=req.params.id;
+    const {name,email,password} = req.body;
+    if(!name && name.trim()=="" && !email && email.trim()==="" && !password && password.length<6){
+        return res.status(422).json({message:"Invalid Data"});
+    }
 
+    let user;
+    try {
+        user=await User.findByIdAndUpdate(id,{name,email,password})
+    } catch (error) {
+        return next(error)
+    }
+    if(!user){
+        return res.status(500).json({message:"Unable to save user"})
+    }
+    return res.status(200).json({message:"Updated Succesfully"})
+}
 
 
 exports.getAllUsers=getAllUsers;
 exports.addUser=addUser;
-
+exports.updateUser =updateUser;
 
 // status 422 means unprocessable entity
 // status 201 used when something is siccesfully addedd
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,10 +1,11 @@
 const express= require("express");
-const { getAllUsers ,addUser} = require("../controller/user-controller");
+const { getAllUsers ,addUser,updateUser} = require("../controller/user-controller");
+
 
 const router= express.Router();
 
 router.get("/",getAllUsers)
 router.post("/", addUser)
-
+router.put("/:id",updateUser)
 
 module.exports=router;
